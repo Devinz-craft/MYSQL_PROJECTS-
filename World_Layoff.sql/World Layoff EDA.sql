50b6c3e3028e686581b272e06260dc75eacddbf1
Daniel Obioha <obiohaugodev@gmail.com>
2026-01-28 16:07:28 +0100

    Delete World_Layoff.sql/World Layoff EDA.sql

--- World_Layoff.sql/World Layoff EDA.sql
+++
@@ -1,7 +0,0 @@
--- EDA --
-
--- Taking note of the timeline in which these layoffs happened -- 
-SELECT Min(`date`), Max(`date`)
-FROM layoffs_sample2;
-
--- The timeline of these layofs is between 2020-2023 -- 
